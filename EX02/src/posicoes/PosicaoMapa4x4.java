ef57c0b88906d62c6c512d7a7be1078edf48e761
ju-vargas <ju.rodrigues0100@gmail.com>
2023-06-01 11:53:18 -0300

    criacao classes

---
+++ EX02/src/posicoes/PosicaoMapa4x4.java
@@ -0,0 +1,55 @@
+package teste;
+
+public class PosicaoMapa4x4 {
+
+    public int x;
+    public int y;
+    public int numPosicoesOcupadas;
+
+    public void PosicaoMapa4x4(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public boolean setX(int x){
+        this.x = x; 
+    }
+
+    public boolean setY(int y){
+        this.y = y; 
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int numPosicoesOcupadas() {
+
+    }
+
+    public PosicaoMapa4x4 copy(){
+
+    }
+
+    public void imprime(){
+
+    }
+
+    public int distancia (PosicaoMapa4x4 p){
+
+    }
+
+    public reset(){
+
+    }
+
+    boolean estaoMesmaPosicao(PosicaoMapa4x4 p1, PosicaoMapa4x4 p2){
+        
+    }
+
+ 
+}
