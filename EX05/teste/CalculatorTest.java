8851f6b71e63b5faf43180abfc85d21a50af3cd3
ju-vargas <ju.rodrigues0100@gmail.com>
2023-06-29 11:55:29 -0300

    init 5

---
+++ EX05/teste/CalculatorTest.java
@@ -0,0 +1,21 @@
+package teste;
+
+import calculator.Calculator;
+
+
+public class CalculatorTest {
+    Calculator calc; 
+
+
+    public void start(){
+        System.out.println("Os testes estão começando"); 
+    
+    }
+
+    public void init(){
+        this.calc = new(Calculator); 
+
+    }
+
+
+}
